54e5ce294e59b17ce1bde61cb9c42c49243120a6
EmmaLangeland <emml@stud.ntnu.no>
2025-05-11 14:19:32 +0200

    shared variable properly C

--- ovinger/E1/shared variable/c/foo.c
+++ ovinger/E1/shared variable/c/foo.c
@@ -5,28 +5,49 @@
 #include <stdio.h>
 
 int i = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 // Note the return type: void*
-void* incrementingThreadFunction(){
+void *incrementingThreadFunction()
+{
+    for (int j = 0; j < 1000001; j++)
+    {
+        pthread_mutex_lock(&mutex);
+        i++;
+        pthread_mutex_unlock(&mutex);
+    }
     // TODO: increment i 1_000_000 times
     return NULL;
 }
 
-void* decrementingThreadFunction(){
+void *decrementingThreadFunction()
+{
+    for (int j = 0; j < 1000000; j++)
+    {
+        pthread_mutex_lock(&mutex);
+        i--;
+        pthread_mutex_unlock(&mutex);
+    }
     // TODO: decrement i 1_000_000 times
     return NULL;
 }
 
-
-int main(){
-    // TODO: 
+int main()
+{
+    // TODO:
     // start the two functions as their own threads using `pthread_create`
     // Hint: search the web! Maybe try "pthread_create example"?
-    
+    pthread_t incrementingThread, decrementingThread;
+
+    pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
+    pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
+
     // TODO:
     // wait for the two threads to be done before printing the final result
-    // Hint: Use `pthread_join`    
-    
+    // Hint: Use `pthread_join`
+    pthread_join(incrementingThread, NULL);
+    pthread_join(decrementingThread, NULL);
+
     printf("The magic number is: %d\n", i);
     return 0;
 }
