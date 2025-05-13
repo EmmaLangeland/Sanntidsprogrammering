cab7e3cfab6150d2f7f30c1b81da19e441b5ac88
EmmaLangeland <emml@stud.ntnu.no>
2025-05-13 15:55:57 +0200

    dunno

--- ovinger/E1/bounded buffer/c/main.c
+++ ovinger/E1/bounded buffer/c/main.c
@@ -22,6 +22,7 @@ struct BoundedBuffer *buf_new(int size)
     buf->buf = rb_new(size);
 
     pthread_mutex_init(&buf->mtx, NULL);
+    
     // TODO: initialize semaphores
     sem_init(&buf->capacity, 0, size);
     sem_init(&buf->numElements, 0, 0);
@@ -42,11 +43,10 @@ void buf_push(struct BoundedBuffer *buf, int val)
 {
     // TODO: wait for there to be room in the buffer
     sem_wait(&buf->capacity);
+
     // TODO: make sure there is no concurrent access to the buffer internals
     pthread_mutex_lock(&buf->mtx);
-
     rb_push(buf->buf, val);
-
     pthread_mutex_unlock(&buf->mtx);
 
     // TODO: signal that there are new elements in the buffer
@@ -57,10 +57,9 @@ int buf_pop(struct BoundedBuffer *buf)
 {
     // TODO: same, but different?
     sem_wait(&buf->numElements);
+
     pthread_mutex_lock(&buf->mtx);
-
     int val = rb_pop(buf->buf);
-
     pthread_mutex_unlock(&buf->mtx);
 
     sem_post(&buf->capacity);
